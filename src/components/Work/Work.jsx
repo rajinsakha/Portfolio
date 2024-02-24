761ea784d26dcbbd6c9dbf6eeee7ce1769f52fcf
Rajin Sakha <rajinsakha07@gmail.com>
2024-02-24 22:42:37 +0545

    changed-cv

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -14,7 +14,7 @@ const Work = ({ imgUrl, title, description, link, index }) => {
           <div className="app__work_details">
             <h3 className="app__work_details-title">{title}</h3>
             <p className="app__work_details-description">{description}</p>
-            <a href={link} target="_blank"  className="custom__button">View Project</a>
+            <a href={link} target="_blank" rel="noreferrer"  className="custom__button">View Project</a>
           </div>
           <div className="app__work_image">
             <img src={imgUrl} alt={`${imgUrl} thumbnail`} />
@@ -28,7 +28,7 @@ const Work = ({ imgUrl, title, description, link, index }) => {
           <div className="app__work_details">
             <h3 className="app__work_details-title">{title}</h3>
             <p className="app__work_details-description">{description}</p>
-            <a href={link} target="_blank" className="custom__button">View Project</a>
+            <a href={link} target="_blank" rel="noreferrer" className="custom__button">View Project</a>
           </div>
         </>
       )}
