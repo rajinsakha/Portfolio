a190b9027e839ba5ade36339dd3caa7df10bc5dd
Rajin Sakha <rajinsakha07@gmail.com>
2024-10-07 14:28:08 +0545

    updated-hero

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <p className='app__header-p'>Front End Developer</p>
         <h1 className='app__header-h1'>Hi, I'm <span className='app__header_highlighted'>Rajin</span></h1>
         <h1 className='app__header-h1'>Sakha From Nepal</h1>
-        <a href={cv} download className='custom__button' style={{marginTop:"2rem"}}>Download CV</a>
+        <a href={cv} download className='custom__button' style={{marginTop:"2rem"}}>Download Resume</a>
       </div>
 
       <div className="app__wrapper_img" id='img-div'>
