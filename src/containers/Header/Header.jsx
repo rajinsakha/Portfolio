577f08a7629511969cdb865cd8318308225e4c29
Rajin Sakha <rajinsakha07@gmail.com>
2024-10-10 14:33:15 +0545

    changes

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import cv from "../../assets/RajinSakha_Resume.pdf"
+
 import './Header.css'
 
 const Header = () => {
@@ -9,7 +9,7 @@ const Header = () => {
         <p className='app__header-p'>Front End Developer</p>
         <h1 className='app__header-h1'>Hi, I'm <span className='app__header_highlighted'>Rajin</span></h1>
         <h1 className='app__header-h1'>Sakha From Nepal</h1>
-        <a href={cv} download className='custom__button' style={{marginTop:"2rem"}}>Download Resume</a>
+        <a href='/RajinSakha_Resume.pdf' target='_blank' rel='noreferrer' className='custom__button' style={{marginTop:"2rem"}}>View Resume</a>
       </div>
 
       <div className="app__wrapper_img" id='img-div'>
